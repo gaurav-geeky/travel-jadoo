15e580492371730188f957b2a6388e1da1e774ab
gaurav-geeky <gauravmpreat@gmail.com>
2025-06-23 23:52:16 +0530

    fetch: PUSH

--- crud.js
+++ crud.js
@@ -1,3 +1,4 @@
+// FETCH DATA     URL    FETCH 
 
 let fetchdt = async () => {
 
@@ -23,17 +24,93 @@ let fetchdt = async () => {
             <td> ${e.Child}</td> 
             <td> ${e.Infant}</td> 
             <td> ${e.Senior}</td> 
+            <td> ${e.Total}</td> 
             <td onclick="Del('${e.id}') "> delete </td> 
         </tr>
     `
     }
     )
 }
-fetchdt();
+// fetchdt();
 
+//   DELETE method 
 let Del = (id)=> { 
     let url = `http://localhost:3000/flight/${id}` 
     fetch(url, {method: "DELETE"})
 }
+
+
+//  POST method 
+let ins = () => { 
+
+    let From = document.querySelector('#fromwhere').value
+    let To = document.querySelector('#towhere').value
+    let Departure = document.querySelector('#depart').value
+    let Return = document.querySelector('#returns').value
+    let Adult = document.querySelector('#adult').value
+    let Child = document.querySelector('#child').value
+    let Infant = document.querySelector('#infant').value
+    let Senior = document.querySelector('#senior').value
+
+    let url = "http://localhost:3000/flight";
+
+    fetch( url, {
+
+        method: "POST", 
+
+        headers: { 
+            "Content-type": "application/json"
+        }, 
+        
+        body: JSON.stringify( 
+
+            {
+                From: From, 
+                To: To, 
+                Departure: Departure, 
+                Return: Return, 
+                Adult: Adult, 
+                Child: Child, 
+                Infant: Infant, 
+                Senior: Senior, 
+                Total: 500*Adult+ 500*Child+ 500*Infant+ 500*Senior, 
+
+                // here key name should be same as tbody td key name
+            }
+        )
+    })   
+    location.href="passDetails.html" 
+
+    return false 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
